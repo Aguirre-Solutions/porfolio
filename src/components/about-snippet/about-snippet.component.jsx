9cb27865fb14c9559a9e2eda7ef49325e1eedeb5
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-10-19 10:49:08 -0600

    Changed about snippet content

--- src/components/about-snippet/about-snippet.component.jsx
+++ src/components/about-snippet/about-snippet.component.jsx
@@ -9,7 +9,7 @@ const AboutSnippet = () => {
             </Box>
             <Box>   
                 <StyledTypography variant="body1" sx={{fontSize:'20px'}}>
-                Hello, I'm Ruben, a junior full stack developer with a unique background in psychology, language education, and entrepreneurship in Mexico. 
+                Hello, I'm Ruben, transitioning into a junior full stack developer role with a unique background in psychology, language education, and entrepreneurship in Mexico. 
                 <br></br> <br></br>
                 I've honed skills in adaptability, clear communication, and fresh innovation. Fluent in multiple languages and with a knack for designing user-friendly digital spaces, I combine empathy and strategy with a continuous drive to learn and streamline.
                 <br></br> 
