bc0f3c015c71ba2353a952f6408318316d72ed71
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-10-24 17:07:05 -0600

    Made custom tool for cardheight Ordered imports

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,14 +1,14 @@
+import { useContext, useState, useEffect } from 'react';
+import { ThemeValuesContext } from '../../context/theme-values.context';
+
 import {Box, Grid} from '@mui/material';
+import {  StyledHorizontalDivider } from '../../utils/styledComponents';
 
 import Portfolio from '../portfolio/portfolio.component';
 import Brief from '../brief/brief.component';
-import { useContext, useState, useEffect } from 'react';
-
-import { ThemeValuesContext } from '../../context/theme-values.context';
-
-import { StyledVerticalDivider, StyledHorizontalDivider } from '../../utils/styledComponents';
-
 import CV from '../cv/cv.component';
+
+import { getCardHeight } from '../../utils/deviceHelpers';
 
 const Home = () => {
 
@@ -21,13 +21,14 @@ const Home = () => {
 
     const [maxSkillCardHeight, setMaxSkillCardHeight] = useState(1050);
     const [maxExperienceCardHeight, setMaxExperienceCardHeight] = useState(1050);
-
+    
     useEffect(() => {
-        isMobile ? setMaxSkillCardHeight('auto') : isTablet ? setMaxSkillCardHeight(900) : isLaptop ? setMaxSkillCardHeight(950) : isDesktop ? setMaxSkillCardHeight(850) : setMaxSkillCardHeight(850)
+        const heightForSkills = getCardHeight(isMobile, isTablet, isLaptop, isDesktop);
+        const heightForExperience = getCardHeight(isMobile, isTablet, isLaptop, isDesktop);
 
-        isMobile ? setMaxExperienceCardHeight('auto') : isTablet ? setMaxExperienceCardHeight(900) : isLaptop ? setMaxExperienceCardHeight(950) : isDesktop ? setMaxExperienceCardHeight(850) : setMaxExperienceCardHeight(850)
-        }
-    , [checks]);
+        setMaxSkillCardHeight(heightForSkills);
+        setMaxExperienceCardHeight(heightForExperience);
+    }, [checks]);
 
 
 
@@ -40,7 +41,7 @@ const Home = () => {
                 <StyledHorizontalDivider sx={{ width:'80%' }} checks={checks}/>
             </Box>
             
-            <CV theme={theme} maxSkillCardHeight={maxSkillCardHeight} maxExperienceCardHeight={maxSkillCardHeight} checks={checks}/>
+            <CV theme={theme} maxSkillCardHeight={maxSkillCardHeight} maxExperienceCardHeight={maxExperienceCardHeight} checks={checks}/>
 
             <Box component={Grid} item xs={12} display="flex" justifyContent="center" alignItems="center">
                 <StyledHorizontalDivider sx={{ margin: '20px', width:'80%' }} />
