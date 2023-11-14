b87e3383063fdf8f9a84830f37afee58e9d59b87
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-11-13 21:21:13 -0600

    Updated project page to use new technologyIcons

--- src/pages/project/project.component.jsx
+++ src/pages/project/project.component.jsx
@@ -3,33 +3,24 @@ import { useContext } from "react";
 import { ProjectDataContext } from "../../context/project-data.component";
 import { ThemeValuesContext } from "../../context/theme-values.context";
 
-import { Grid, Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import { StyledHorizontalDivider } from "../../utils/styledComponents";
 
 import ProjectOverview from "../../components/project-overview/project-overview.component";
 import ProjectDetailed from "../../components/project-detailed/project-detailed.component";
-import {SiReact,SiMui, SiFirebase, SiMongodb} from 'react-icons/si';
-import {FaNodeJs} from 'react-icons/fa';
-import cloudinaryIconSrc from '../../assets/cloudinary.svg';
 
 
 const Project = () => {
     const { id } = useParams();
-    const projects = useContext(ProjectDataContext);  
+    const {projectsData, technologyIcons} = useContext(ProjectDataContext);  
     const {checks, theme} = useContext(ThemeValuesContext);
-    const project = projects.find((p) => p.id === parseInt(id, 10));
+    const project = projectsData.find((p) => p.id === parseInt(id, 10));
 
     const { isMobile, isTablet, isLaptop, isDesktop } = checks;
     
-    const technologyIcons = {
-        'React': <SiReact />,
-        'Node.js': <FaNodeJs />,
-        'Firebase': <SiFirebase />,
-        'Material UI': <SiMui />,
-        'Cloudinary': <img src={cloudinaryIconSrc} alt="Cloudinary" style={{ width: '24px', height: '24px' }} />,
-        'MongoDB': <SiMongodb />
-      };
+
       
+    
     
     
     return (   
