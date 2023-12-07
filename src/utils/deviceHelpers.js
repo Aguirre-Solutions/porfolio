42b2e676f138d7ef7d4ed53a09cbdc37e70e6ef5
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2023-12-07 13:20:11 -0600

    Better determined the height of cards

--- src/utils/deviceHelpers.js
+++ src/utils/deviceHelpers.js
@@ -1,7 +1,7 @@
 export const getCardHeight = (isMobile, isTablet, isLaptop, isDesktop) => {
     if(isMobile) return 'auto';
-    if(isTablet) return 950;
-    if(isLaptop) return 950;
+    if(isTablet) return 850;
+    if(isLaptop) return 900;
     if(isDesktop) return 900;
     return 850;
 };
