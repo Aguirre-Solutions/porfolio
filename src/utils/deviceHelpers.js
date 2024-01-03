5bae6b4aaa66948913dc027906f743245b3c5509
Ruben Aguirre <rubenaguirrelizcano@gmail.com>
2024-01-02 18:51:35 -0600

    modified tablet cv height

--- src/utils/deviceHelpers.js
+++ src/utils/deviceHelpers.js
@@ -1,6 +1,6 @@
 export const getCardHeight = (isMobile, isTablet, isLaptop, isDesktop) => {
     if(isMobile) return 'auto';
-    if(isTablet) return 850;
+    if(isTablet) return 900;
     if(isLaptop) return 900;
     if(isDesktop) return 900;
     return 850;
